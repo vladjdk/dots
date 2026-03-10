c5dad0e4ed6cb7f4f6691c9a7599e881d66de70c
Vlad <vladjdk@gmail.com>
2026-03-09 23:14:17 -0400

    add install or update checker to install script

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@
 
 DOTS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-xcode-select --install
+# Xcode tools (only if not installed)
+xcode-select -p &>/dev/null || xcode-select --install
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Oh My Zsh (only if not installed)
+[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Homebrew (only if not installed)
+command -v brew &>/dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Dotfiles
 cp "$DOTS_DIR/dots/.zshrc" ~
@@ -23,21 +27,37 @@ cp -r "$DOTS_DIR/nvim" ~/.config/nvim
 brew update
 brew upgrade
 
+install_or_upgrade() {
+  if brew list "$1" &>/dev/null; then
+    brew upgrade "$1"
+  else
+    brew install "$1"
+  fi
+}
+
+install_or_upgrade_cask() {
+  if brew list --cask "$1" &>/dev/null; then
+    brew upgrade --cask "$1"
+  else
+    brew install --cask "$1"
+  fi
+}
+
 # CLI tools
-brew install git
-brew install neovim
-brew install tmux
-brew install lazygit
-brew install fzf
-brew install ripgrep
-brew install fd
-brew install go
-brew install node
-brew install yarn
-brew install nvm
+install_or_upgrade git
+install_or_upgrade neovim
+install_or_upgrade tmux
+install_or_upgrade lazygit
+install_or_upgrade fzf
+install_or_upgrade ripgrep
+install_or_upgrade fd
+install_or_upgrade go
+install_or_upgrade node
+install_or_upgrade yarn
+install_or_upgrade nvm
 
 # Apps
-brew install --cask ghostty
-brew install --cask orbstack
-brew install --cask goland
-brew install --cask bitwarden
+install_or_upgrade_cask ghostty
+install_or_upgrade_cask orbstack
+install_or_upgrade_cask goland
+install_or_upgrade_cask bitwarden
